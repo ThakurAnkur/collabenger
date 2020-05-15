e79a0a15713d06b3e32bb697995d21a97220ff26
ThakurAnkur <thakur.ankur049@gmail.com>
2020-05-15 22:42:06 +0530

    CROS enable

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+var cors = require('cors')
 
 var app = express();
 
@@ -20,6 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+app.options('*', cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
